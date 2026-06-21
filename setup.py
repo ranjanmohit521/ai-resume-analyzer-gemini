058ca47425e75c486e03f11c647a9a48d461c4fe
Mohit Ranjan <ranjanmohit2005@gmail.com>
2026-06-21 21:19:36 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-name="ai-resume-analyzer",
-version="1.0.0",
-description="AI-powered Resume Analyzer using Gemini API and Streamlit",
-author="Mohit Ranjan",
-packages=[],
-)
